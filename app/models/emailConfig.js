d6020e42e1b07e21ef8df07e9e0c95d19370da0f
Nicholas Womble <new6584@rit.edu>
2016-09-21 18:35:35 -0400

    refactored to seperate concerns a bit

---
+++ app/models/emailConfig.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var staticRoot = path.dirname(require.main.filename);
+var schemas = require('./models');
+console.log(staticRoot);
+module.exports = function(mongoose){//needs an instance of 
+    var emailer = require('email-verification')(mongoose);
+
+    emailer.configure({
+        verificationURL: staticRoot+'email-verification?id=${URL}',
+        persistentUserModel: schemas.User,
+        tempUserCollection: 'razu_TempUser',
+
+        transportOptions: {
+            service: 'Gmail',
+            auth: {
+                user: 'Razu.Verifier@gmail.com',
+                pass: '~RazuMusic~'
+            }
+        },
+        verifyMailOptions: {
+            from: 'Do Not Reply <Razu.Verifier_do_not_reply@gmail.com>',
+            subject: 'Please confirm Razu account',
+            html: 'Click the following link to confirm your account:</p><a href="'+staticRoot+'${URL}">${URL}</a></p>',
+            text: 'Please confirm your account by clicking the following link: ${URL}'
+        }
+    }, function(error, options){
+        if(error){
+            console.log(error);
+        }
+    });
+
+    var TempUser = schemas.TempUser;
+    emailer.configure({
+        tempUserModel: TempUser
+    }, function(error, options){
+        if(error){
+            console.log(error);
+        }
+    });
+
+    return emailer;
+};
--- app/routes/api.js
+++ app/routes/api.js
@@ -1,15 +1,136 @@
-var express = require('express'); //server side paths
+
+var express = require('express');//response.render('a.html')
 var router = express.Router();
 var path = require('path');
-/* GET home page. */
-router.get('/login', function(req, res, next) {
-   console.log("received");
-   console.log(res); 
-   res.render('index.html');
+
+var schemas = require('../models/models'),
+    mongoose = require('mongoose'),
+    emailer = require('../models/emailConfig.js')(mongoose);
+    sjcl = require('../../library/sjcl/sjcl.js');
+
+var User = schemas.User,
+    encryptKey = makeRandomWord();
+
+router.post('/register', function(request,response){
+    var myUsername = request.body.username;
+    var myEmail = request.body.email;
+    var myDisplayname = request.body.displayName;
+    var mySalt = sjcl.codec.base64.fromBits(sjcl.random.randomWords(10));
+    //var mySalt = Buffer.alloc(rawSalt.length, rawSalt, 'base64');
+    //hash password
+    var myPassword = saltyHash(request.body.password, mySalt);
+    var newUser= new User({
+        username: myUsername,
+        password: myPassword,
+        displayName: myDisplayname,
+        email: myEmail,
+        salt: mySalt
+    });
+    emailer.createTempUser(newUser,function(err, existingPersistentUser, newTempUser){
+         if (err){
+             console.log(err);
+             return;
+         }
+        // user already exists in persistent collection... 
+        if (existingPersistentUser){
+            sendToClient(response,{error:'taken'});
+            console.log('register- info taken');
+            return;
+        }
+        // a new user 
+        if (newTempUser) {
+            var URL = newTempUser[emailer.options.URLFieldName];
+            emailer.sendVerificationEmail(myEmail, URL, function(err, info) {
+                if (err){
+                   console.log(err); 
+                }
+                console.log('sucess verification');
+                sendToClient(response,{verified:true});
+            });
+        } else {
+            console.log('temp user exists');
+            sendToClient(response,{error:'taken'});
+        }
+    });
+});//end /register
+
+router.get("/email-verification*",function(request,response){
+    var url = request.query.id;
+    emailer.confirmTempUser(url,function(err,user){
+        if(err){
+            console.log(err);
+        }
+        if(user){
+            emailer.sendConfirmationEmail(user['email'],function(err,info){
+                //redirect
+                console.log("got here");
+                //response.redirect(staticRoot+"working");
+            });
+        }else{
+            //redirect 
+            console.log('redir to signup');
+        }
+
+    });
 });
-// router.get('/okay',function(req,res,next){
-//    console.log("received");
-//    console.log(res); 
-// }); dunno what this does???? 
+
+router.post('/nossl',function(request,response){
+    sendToClient(response,{userVal:encryptKey});
+});
+router.post('/login', function(request,response){
+    //which name varification they wanna use
+    var selectObj;
+    if(request.body.username){
+        selectObj = {username: request.body.username};
+    }else if(request.body.email){
+        selectObj = {email: request.body.email};
+    }
+
+    //didnt send either option
+    if(!selectObj){
+        sendToClient(response,{error:"no_username"});
+        console.log("no credentials")
+        return;
+    }
+
+    //check the db
+    User.find(selectObj,function(err,user){
+        if(err){ console.log(err); }
+        if(user.length != 1){
+            console.log('no user of with info: '+selectObj[1]);
+            sendToClient(response,{error:"username_dne"});
+            return; 
+        }
+        var myPass = saltyHash(request.body.password, user[0].salt);
+
+        if(myPass == user[0].password){
+            //TODO: start passport, redirect user
+            console.log("logged in");
+        }else{
+            console.log("bad pass");
+            sendToClient(response,{error:"wrong_password"});
+        }
+
+    });
+});
+
+//HELPER FUNCTIONS//
+function saltyHash(pass, codedSalt){
+    var plaintext = sjcl.decrypt(encryptKey, pass);
+    var mySalt = sjcl.codec.base64.toBits(codedSalt);
+    var hKey =sjcl.misc.pbkdf2(plaintext, mySalt, 1000, 256);
+    return sjcl.codec.base64.fromBits(hKey);
+}
+function makeRandomWord(){
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for( var i=0; i < 25; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+}
+function sendToClient(response, obj){
+    response.send(obj);
+}
+
 
 module.exports = router;
